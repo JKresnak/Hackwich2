6cc0e3de449ee8f6c1102f2b348eb6bb13a09521
Jkresnak <cmstudent@uhwo-acm-01.uhwo.hawaii.edu>
2020-01-30 10:33:49 -1000

    Completed Parts 4-6

---
+++ Hackwich2/SecondviewcontrollerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondviewcontrollerViewController.swift
+//  Hackwich2
+//
+//  Created by CM Student on 1/30/20.
+//  Copyright © 2020 CM Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondviewcontrollerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ChangeColorButtonPress(_ sender:Any)
+    {
+        self.view.backgroundColor = UIColor.blue
+        
+}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
